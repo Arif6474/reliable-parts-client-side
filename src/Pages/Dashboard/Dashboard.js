f6e4f3205014dfe65eb141a1dda6d5500f17c6cc
Arif6474 <arifulislam64743@gmail.com>
2022-05-26 02:05:23 +0600

    admin can make an admin

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
-
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin'
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
   <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -16,7 +20,7 @@ const Dashboard = () => {
       {/* <!-- Sidebar content here --> */}
       <li><Link to="/dashboard"><button class="btn btn-xs mt-4"> My Orders</button> </Link></li>
       <li><Link to="/dashboard/review"><button class="btn btn-xs mt-4">Add a review</button> </Link></li>
-      <li><Link to="/dashboard/users"><button class="btn btn-xs mt-4">All Users</button> </Link></li>
+     {admin && <li> <Link to="/dashboard/users"><button class="btn btn-xs mt-4">All Users</button> </Link></li>}
     </ul>
   
   </div>
